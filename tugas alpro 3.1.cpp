d80a56d516fe98f659c16df915b58458a0c7150a
SitiHerawati1807 <114640259+SitiHerawati1807@users.noreply.github.com>
2022-10-22 03:28:40 +0700

    mengumpulkan tugas alpro 3
    nomor 1 sampai 4 di pisah

---
+++ tugas alpro 3.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main (){
+    float A = 10,B = 8,C;
+    cout << "mencari sisi C " << endl;
+    cout << "sisi A = 10\n";
+    cout << "sisi B = 8\n";
+    C = sqrt(pow(A,2)+pow(B,2));
+    cout << "hasil sisi C = " << C;
+
+//A = tinngi, B = alas, C = sisi miring
+
+
+    return 0;
+
+
+}
